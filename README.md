550d75a238cdab8a04dd581865c055c0e211ce3a
omesh-barhate <barhateomesh@gmail.com>
2023-05-27 02:35:59 +0530

    updated

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # st-paul-application-form
+
+
+## current status
+
+two pages ready of login screen 
+login and register
+date - 26 /5/23
+
---
+++ form/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
+
+# Create your views here.
+
+def login(request):
+    return render(request,"login.html")
+
+def register(request):
+    return render(request,"register.html")
